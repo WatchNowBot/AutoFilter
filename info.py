91c8558f5e357925b044457b730515528311b638
WatchNowBot <105361018+WatchNowBot@users.noreply.github.com>
2025-06-07 23:01:12 +0530

    info.py

--- info.py
+++ info.py
@@ -13,20 +13,20 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '21484'))
-API_HASH = environ.get('API_HASH', 'db6286a041adf07ef4244e')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7807I9ygow9sLqNQuI7hsWPm5FLNfBr9c')
+API_ID = int(environ.get('API_ID', '9613996'))
+API_HASH = environ.get('API_HASH', 'c02cb5be077027d44609486c0ac1fbc6')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5370534354:AAF2QWGHhRSocekwCOYfSiGfSisXApin_y8')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5016109398').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002190681742'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002190681742').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1729487047').split()]
+USERNAME = environ.get('USERNAME', "https://t.me/letswatchrequest") # ADMIN USERNAME
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001717087987'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/letswatchnow01')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001750371143').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv:learningbajority&appName=learningbots")
-DATABASE_NAME = environ.get('DATABASE_NAME', "learningbots")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://case01f:zPi6Y2ZhtenyRwPf@watchit.ghjaoxy.mongodb.net/?retryWrites=true&w=majority&appName=Watchit")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Watchit")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
@@ -39,13 +39,13 @@ auth_channel = environ.get('AUTH_CHANNEL', '')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
 request_channel = environ.get('REQUEST_CHANNEL', '0')
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '0'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/') #Support group link ( make sure bot is admin )
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/letswatchitnow') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 IS_VERIFY = is_enabled('IS_VERIFY', True)
 #---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/")
-VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
+VERIFY_IMG = environ.get("VERIFY_IMG", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bigsmall.in%2Fblogs%2Funique-gifts%2F20-shows-movies-to-binge-watch-over-the-weekend&psig=AOvVaw3ctKTx4Hn_brmA_Glui0Fb&ust=1749402747957000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIiJiaLn340DFQAAAAAdAAAAABAE")
 SHORTENER_API = environ.get("SHORTENER_API", "3097623f852197a9ce40d1212aaa8bbf2803e799")
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'omegalinks.in')
 SHORTENER_API2 = environ.get("SHORTENER_API2", "3097623f852197a9ce40d1212aaa8bbf2803e799")
@@ -68,11 +68,11 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-START_IMG = (environ.get('START_IMG', 'https://i.ibb.co/qpxpGmC/image.jpg https://i.ibb.co/DQ35zLZ/image.jpg')).split()
-FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
-REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
-PAYPICS = (environ.get('PAYPICS', 'https://envs.sh/_kA.jpg')).split()
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/9f3f47c690bbcc67633c2.jpg'))
+START_IMG = (environ.get('START_IMG', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bigsmall.in%2Fblogs%2Funique-gifts%2F20-shows-movies-to-binge-watch-over-the-weekend&psig=AOvVaw3ctKTx4Hn_brmA_Glui0Fb&ust=1749402747957000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIiJiaLn340DFQAAAAAdAAAAABAE')).split()
+FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bigsmall.in%2Fblogs%2Funique-gifts%2F20-shows-movies-to-binge-watch-over-the-weekend&psig=AOvVaw3ctKTx4Hn_brmA_Glui0Fb&ust=1749402747957000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIiJiaLn340DFQAAAAAdAAAAABAE')
+REFER_PICS = (environ.get("REFER_PICS", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bigsmall.in%2Fblogs%2Funique-gifts%2F20-shows-movies-to-binge-watch-over-the-weekend&psig=AOvVaw3ctKTx4Hn_brmA_Glui0Fb&ust=1749402747957000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIiJiaLn340DFQAAAAAdAAAAABAE")).split() 
+PAYPICS = (environ.get('PAYPICS', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bigsmall.in%2Fblogs%2Funique-gifts%2F20-shows-movies-to-binge-watch-over-the-weekend&psig=AOvVaw3ctKTx4Hn_brmA_Glui0Fb&ust=1749402747957000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIiJiaLn340DFQAAAAAdAAAAABAE')).split()
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bigsmall.in%2Fblogs%2Funique-gifts%2F20-shows-movies-to-binge-watch-over-the-weekend&psig=AOvVaw3ctKTx4Hn_brmA_Glui0Fb&ust=1749402747957000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIiJiaLn340DFQAAAAAdAAAAABAE'))
 REACTIONS = ["👀", "😱", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
